98b7fe423e3e529ea10eec4261d8b71c21819946
Kevin Yancy CodeCrew <40698421+Kevin-CodeCrew@users.noreply.github.com>
2019-09-12 15:12:34 -0500

    Update js.js

--- js.js
+++ js.js
@@ -8,5 +8,5 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 
 /* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it change it back */
 
-CHALLENGE
+// CHALLENGE
 /* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
